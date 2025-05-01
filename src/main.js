34e7bc5dea2b16615ddf26da78f6bafecddbd1b4
SantiagoVSR <73039684+SantiagoVSR@users.noreply.github.com>
2025-04-30 23:22:18 -0500

    Tabla con jQuery y DataTables

--- src/main.js
+++ src/main.js
@@ -2,5 +2,27 @@ import $ from 'jquery';
 import dt from 'datatables.net-dt';
 
 $(document).ready(function () {
-  $('#tabla-ejemplo').DataTable();
+  const mi_tabla = $('#tabla-ejemplo').DataTable();
+
+
+  var url="https://pkgstore.datahub.io/core/continent-codes/continent-codes_json/data/60d6baef1250bc2b01fd0148dccca518/continent-codes_json.json";
+  $("#ObtenerInformacion" ).click(function(){
+    $.ajax({
+      dataType: 'json',
+      url: url
+    }).then(function(data){
+
+      data.forEach(function (item){
+        mi_tabla.row.add([
+          item.Code,
+          item.Name
+        ]).draw(false);
+      });
+    
+    });
+  });
+
+  $("#LimpiarInformacion").click(function (){
+    mi_tabla.clear().draw();
+  });
 });
